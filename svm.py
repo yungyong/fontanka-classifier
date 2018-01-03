90b12a7a45b7a24011007c51bb3e411dd688996d
Anisimova Anna <lacrimayungyong@gmail.com>
2018-01-04 02:04:40 +0300

    updated svm

--- svm.py
+++ svm.py
@@ -1,15 +1,14 @@
 import numpy as np
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 from sklearn.model_selection import cross_val_score
 
 
 def main():
-    svm = SVC(kernel='linear')
+    svm = LinearSVC(max_iter=200, random_state=42)
     X = np.load('./data/X.npy')
     Y = np.load('./data/Y.npy')
-    scores = cross_val_score(svm, X, Y, cv=5)
+    scores = cross_val_score(svm, X, Y, cv=5, verbose=1)
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-
 
 
 if __name__ == '__main__':
